bd582852b43e25c745bfd31e5361ce7c29fbf141
Anatolii Veselyi <anatoliiv1990@gmail.com>
2023-06-26 11:35:10 +0300

    frontend

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+function Footer() {
+  return (
+   
+    <footer>
+        <Container>
+            <Row>
+                <Col className="text-center">Copyright &copy; Holden-Auto 2023</Col>
+            </Row>
+           
+        </Container>
+    </footer>
+    
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Container, Row, Nav, Navbar } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
+
+function Header() {
+  return (
+    <header>
+        <Navbar expand="lg" className="bg-body-dark" bg="dark" data-bs-theme="dark">
+      <Container>
+        <LinkContainer to='/'>
+            <Navbar.Brand>
+            <img
+              alt=""
+              src="../logoHA.png"
+              width="200"
+              height="60"
+              className="d-inline-block align-centr"
+            />{' '}
+            </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer to='/cart'>
+                <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/login'>
+                <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    </header>
+   
+  )
+}
+
+export default Header
